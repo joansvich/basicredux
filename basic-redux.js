e9aefd0a0612dc5960a1e00b54d3c7fcbce2e8f9
Joan Sánchez <joansvich@gmail.com>
2019-07-12 14:27:03 +0200

    Update

--- basic-redux.js
+++ basic-redux.js
@@ -1,24 +1,40 @@
 const redux = require('redux');
- 
+
 // Creamos el store de Redux
 const createStore = redux.createStore;
 
 // state inicial
 const initialState = {
-  
+  usuarios: []
 }
 
 // Reducer
 // parametros: stateActual y accion
-const rootReducer = (state, action) => {
+const rootReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'ADD_USER':
+      return {
+        ...state,
+        usuarios: action.name
+      }
+      break;
+    case 'SHOW_USER':
+      return {
+        ...state
+      }
+  }
   return state;
 }
 
 
-// store --> almacena el state de la aplicación
-// createStore() --> toma 3 parametros: reducer, state inicial, middleware
+// STORE --> almacena el state de la aplicación
+// createStore() --> toma 3 parametros: reducer(obligatorio), state inicial, middleware
 // reducer --> define como cambia el state
 const store = createStore(rootReducer);
+console.log(store.getState());
 
-
+// Dispatch: es la forma de cambiar el state
+store.dispatch({ type: 'ADD_USER', name: 'Joan' });
+console.log(store.getState());
+store.dispatch({ type: 'SHOW_USER' });
 console.log(store.getState());
