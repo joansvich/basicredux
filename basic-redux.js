34b13b19692aa54234569bf166aff5cba9b5760a
Joan Sánchez <joansvich@gmail.com>
2019-07-12 12:39:53 +0200

    first commit

--- basic-redux.js
+++ basic-redux.js
@@ -3,12 +3,22 @@ const redux = require('redux');
 // Creamos el store de Redux
 const createStore = redux.createStore;
 
+// state inicial
+const initialState = {
+  
+}
+
+// Reducer
+// parametros: stateActual y accion
+const rootReducer = (state, action) => {
+  return state;
+}
 
 
 // store --> almacena el state de la aplicación
 // createStore() --> toma 3 parametros: reducer, state inicial, middleware
 // reducer --> define como cambia el state
-const store = createStore();
+const store = createStore(rootReducer);
 
 
 console.log(store.getState());
